f60432dccf554a4d1909193bc4a8a46c71b72db4
graham.stonebridge <graham.stonebridge@exactearth.com>
2016-09-02 10:18:56 -0400

    Created  empty sbt project

---
+++ build.sbt
@@ -0,0 +1,8 @@
+import Dependencies.deps
+
+name := "Point3D_LLA_conversion_example"
+version := "1.0"
+scalaVersion := "2.11.8"
+
+
+
---
+++ project/Dependencies.scala
@@ -0,0 +1,7 @@
+import sbt._
+object Dependencies{
+  val deps = Seq{
+    "org.orekit"           %  "orekit"             % "8.0"
+  }
+}
+
